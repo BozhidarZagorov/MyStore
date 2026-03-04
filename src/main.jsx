4cc616051142fcdb7c0b604d172e4fcf4d1f7090
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-03-04 17:43:50 +0200

    import css styles separated
    separate for easier reading and maintenance

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
+import './Styles/styles.css'
+import './Styles/cart.css'
+import './Styles/footer.css'
+import './Styles/header.css'
+import './Styles/home.css'
+import './Styles/legal.css'
+import './Styles/plp.css'
+import './Styles/productCard.css'
+import './Styles/toast.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
